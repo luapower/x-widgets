e5f8df79176b40602dc5a00b60862b919c11a8c5
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2021-10-26 02:40:06 +0300

    unimportant

--- glue.js
+++ glue.js
@@ -755,36 +755,6 @@ function freelist(create, init, destroy) {
 	return e
 }
 
-// dynamic array with stable element indices.
-function stable_index_array(I) {
-	I = I || 'i'
-	let e = []
-	let free = []
-	e.add = function(e) {
-		let i = free.pop()
-		if (i == null)
-			i = this.length
-		this[i] = e
-		e[I] = i
-		return e
-	}
-	e.remove = function(e) {
-		assert(e[I] != null)
-		free.push(e[I])
-		this[i] = undefined
-		e[I] = null
-		return e
-	}
-	e.clear = function() {
-		for (let e of this)
-			if (e)
-				e[I] = null
-		this.length = 0
-		free.length = 0
-	}
-	return e
-}
-
 // stack with freelist.
 function freelist_stack(create, init, destroy) {
 	let e = {}
